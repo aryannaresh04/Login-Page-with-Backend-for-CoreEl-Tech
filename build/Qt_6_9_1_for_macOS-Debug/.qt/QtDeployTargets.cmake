97ff1fabcba0eb7c9ecf6ec4a5110f6750cae57e
Aryan Naresh <aryannaresh@gmail.com>
2025-07-27 22:48:38 +0530

    Removed build files from version control

--- build/Qt_6_9_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_LoginPage_FILE /Users/Aryan/QT Creator Files/LoginPage/build/Qt_6_9_1_for_macOS-Debug/LoginPage.app/Contents/MacOS/LoginPage)
-set(__QT_DEPLOY_TARGET_LoginPage_TYPE EXECUTABLE)
